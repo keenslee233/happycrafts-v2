8bb7e59d32e5c6628776f052ded4259483aa37c5
EmmyCodes234 <emmycodes234@gmail.com>
2026-03-26 08:43:11 +0100

    fix Netlify 404 error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin-react-router";
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
@@ -49,7 +50,7 @@ export default defineConfig({
       allow: ["app", "node_modules"],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), netlify()],
   build: {
     assetsInlineLimit: 0,
   },
